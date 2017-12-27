f845b9fb8fcbf5e559ef61676092fbf392267d54
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-27 00:11:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ function updateObjectWithKeyAndValue(object,key,value) {
 }
 
 function updateObjectWithKeyAndValue(object,key,value) {
-  object[key] = value
-  return object
+  return Object.Assign({}, object, {key:value})
 }
