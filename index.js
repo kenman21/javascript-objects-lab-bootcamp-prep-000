c4c82ecf51265a4b1ce42381da85575cf86160b2
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-27 00:21:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newobj = Object.assign({}, object, {key:value})
+  var newobj = Object.assign({}, object, {[key]:value})
   return newobj
 }
