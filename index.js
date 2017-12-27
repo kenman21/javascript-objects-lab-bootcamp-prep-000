e8839bfc18258df926f9e42d40451de80b739f9c
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-27 00:27:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ function deleteFromObjectByKey(object,key) {
   return newobj
 }
 
-destructivelyDeleteFromObjectByKey(object,key) {
+function destructivelyDeleteFromObjectByKey(object,key) {
+  delete object[key]
+  return object
+}
