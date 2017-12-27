8c411e0e4dbc83c83068f9dee52a01b26114b4ac
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-27 00:26:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key, value) {
 }
 
 function deleteFromObjectByKey(object,key) {
-  var newobj = Object.assign({},object,{[key]:value})
+  var newobj = Object.assign({},object)
   delete newobj[key]
   return newobj
 }
